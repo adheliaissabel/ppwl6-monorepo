6039a3d2b15242f8abc9542431d6f3dd9c9f844b
adheliaissabel <h1101241001@student.untan.ac.id>
2026-03-12 13:16:27 +0700

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,21 @@
+// backend/src/index.ts
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
+import type { ApiResponse, HealthCheck } from "shared";
+
+const app = new Elysia()
+  .use(cors({ origin: ["http://localhost:5173"] }))
+  .use(swagger())
+  .get("/", (): ApiResponse<HealthCheck> => {
+    return {
+      data: { status: "ok" },
+      message: "server running"
+    };
+  })
+  .listen(3000);
+
+export type App = typeof app;
+
+console.log(`🦊 Backend → http://localhost:${app.server?.port}`);
+console.log(`📖 Swagger → http://localhost:${app.server?.port}/swagger`);
