3efcb8710f17e70dff37fc509411e5c06669527d
adheliaissabel <h1101241001@student.untan.ac.id>
2026-03-15 13:27:39 +0700

    update

--- README.md
+++ README.md
@@ -13,3 +13,19 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.3.8. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
+
+# Monorepo Project
+
+Tech Stack
+- Bun
+- React
+- Vite
+- Tailwind
+- ShadCN
+- Elysia
+- Prisma
+
+Run project
+
+bun install
+bun dev
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,21 +1,31 @@
-// backend/src/index.ts
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
-import type { ApiResponse, HealthCheck } from "shared";
+import { PrismaClient } from "./generated/prisma/client";
+import { prisma } from '../prisma/db';
+import type { ApiResponse, HealthCheck, User } from "shared";
 
 const app = new Elysia()
-  .use(cors({ origin: ["http://localhost:5173"] }))
+  // Pastikan port frontend kamu sudah benar di sini (misal 5173 atau 5174)
+  .use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"] }))
   .use(swagger())
   .get("/", (): ApiResponse<HealthCheck> => {
     return {
       data: { status: "ok" },
       message: "server running"
-    };
+    }
+  })
+  // 👇 INI BAGIAN PENTINGNYA, PASTIKAN ADA ROUTE INI 👇
+  .get("/users", async () => {
+    const users = await prisma.user.findMany()
+    const response: ApiResponse<User[]> = {
+      data: users,
+      message: "User list retrieved"
+    }
+    return response
   })
   .listen(3000);
 
-export type App = typeof app;
-
-console.log(`🦊 Backend → http://localhost:${app.server?.port}`);
-console.log(`📖 Swagger → http://localhost:${app.server?.port}/swagger`);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
