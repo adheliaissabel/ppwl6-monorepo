bbe42d54132d00da10a8be25a5fb40c3c87395ca
adheliaissabel <h1101241001@student.untan.ac.id>
2026-03-24 12:38:10 +0700

    ya Allah

--- apps/backend/prisma/db.ts
+++ apps/backend/prisma/db.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 
-const adapter = new PrismaLibSql({ url: process.env.DATABASE_URL || "" });
+const adapter = new PrismaLibSql({
+  url: process.env.DATABASE_URL || "",
+});
+
 export const prisma = new PrismaClient({ adapter });
